6c060575ddd8979af6301b3342f721cf734e9bdc
Artem <artemm3note@gmail.com>
2024-10-03 14:27:57 +0300

    Added DBContext. Applied migrations.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,6 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 DotNetEnv.Env.Load();
@@ -5,6 +8,20 @@ DotNetEnv.Env.Load();
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+    connectionString = connectionString?
+        .Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST"))
+        .Replace("${DB_PORT}", Environment.GetEnvironmentVariable("DB_PORT"))
+        .Replace("${DB_DATABASE}", Environment.GetEnvironmentVariable("DB_DATABASE"))
+        .Replace("${DB_USERNAME}", Environment.GetEnvironmentVariable("DB_USERNAME"))
+        .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
+
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
