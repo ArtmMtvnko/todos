38cac2027db15a6c3487d0b2cc08b6e7febf90dd
Artem <artemm3note@gmail.com>
2024-10-07 18:29:10 +0300

    Added window for todos.

---
+++ frontend/src/app/components/window/window.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'window',
+    standalone: true,
+    templateUrl: './window.component.html',
+    styleUrl: './window.component.scss',
+})
+export class WindowComponent {}
