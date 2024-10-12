ed43a4d02dead65e120d1a0dab68f88ddcc82e53
Artem <artemm3note@gmail.com>
2024-10-12 23:27:48 +0300

    Added login form (UI).

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TodosTabComponent } from './components/todos-tab/todos-tab.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: TodosTabComponent },
+    { path: 'login', component: LoginComponent}
 ];
