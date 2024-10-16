1eba607110b4ff1c905f5c66b98c532f84a056ed
Artem <artemm3note@gmail.com>
2024-10-16 17:52:21 +0300

    Added search bar in UI. (without functionality.)

--- frontend/src/app/components/todos-tab/todos-tab.component.ts
+++ frontend/src/app/components/todos-tab/todos-tab.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CategoriesComponent } from '../categories/categories.component';
 import { TodosComponent } from '../todos/todos.component';
 import { UserInfoComponent } from '../user-info/user-info.component';
+import { SearchBarComponent } from "../search-bar/search-bar.component";
 
 @Component({
     selector: 'todo-tab',
     standalone: true,
-    imports: [UserInfoComponent, CategoriesComponent, TodosComponent],
+    imports: [UserInfoComponent, SearchBarComponent, CategoriesComponent, TodosComponent],
     templateUrl: './todos-tab.component.html',
     styleUrl: './todos-tab.component.scss',
 })
